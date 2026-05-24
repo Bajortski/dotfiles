d2148cfd50d18eea39ae61dd44af438c82185c0a
Bajortski <65676366+Bajortski@users.noreply.github.com>
2026-05-24 14:00:17 -0400

    modified lualine config

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -38,9 +38,8 @@ return {
       lualine_a = { "mode" },
       lualine_b = { "branch", "diff", "diagnostics" },
       lualine_c = { "filename" },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
+      lualine_x = { "encoding", "filetype" },
+      lualine_y = { "location" },
     },
     inactive_sections = {
       lualine_a = {},
