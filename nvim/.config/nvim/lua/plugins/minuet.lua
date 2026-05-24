b0776d4a138be62d22a71a217c7e04e3555c4a15
Bajortski <65676366+Bajortski@users.noreply.github.com>
2026-05-24 12:53:26 -0400

    modified minuet config

--- nvim/.config/nvim/lua/plugins/minuet.lua
+++ nvim/.config/nvim/lua/plugins/minuet.lua
@@ -3,7 +3,8 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     require('minuet').setup {
-      request_timeout = 10,
+			notify = 'warn',
+      request_timeout = 30,
       provider = 'openai_compatible',
       virtualtext = {
         auto_trigger_ft = { 'markdown' },
@@ -15,6 +16,10 @@ return {
           dismiss = '<A-e>',
         },
       },
+			n_completions = 1,
+			context_window = 512,
+			throttle = 100,
+			debounce = 100,
       provider_options = {
         openai_compatible = {
           model = 'mistral:7b',
@@ -24,7 +29,7 @@ return {
           stream = false,
           system = {
             prompt = [[
-You are a writing assistant. Predict the next 1-5 words of the given English text naturally, maintaining the author's voice and style. Provide only the completion text itself — no explanations, commentary, or markdown formatting. Keep completions concise, short, and in one line.
+You are a writing assistant completing writing in the style of the author. Predict the next sentence (or end of the current sentence if it makes sense) of the given English text naturally, maintaining the author's voice and style. Avoid hedging language, filler phrases and sentimental constructions. Continue the existing line of thought. Provide only the completion text itself, no commentary. Do not write lists. Keep your completions to a single line (do not ever make new lines) and one sentence. Follow grammatical conventions when starting a sentence, do not capitalize your completions if you are not starting a sentence. Do not use the Oxford comma.
             ]],
           },
           optional = {
