28369544f14c9208ad7cb137f9b75d34a07632e9
Bajortski <65676366+Bajortski@users.noreply.github.com>
2026-06-04 00:47:14 -0400

    updated obsidian.lua

--- nvim/.config/nvim/lua/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/plugins/obsidian.lua
@@ -2,34 +2,34 @@ return {
   "obsidian-nvim/obsidian.nvim",
   version = "*",
   ft = "markdown",
-
   opts = {
     -- explicit defaults
     legacy_commands = false,
     frontmatter = {
       enabled = false,
     },
-    -- where to put new notes
-    new_notes_location = "current_dir",
-
+    -- use wikilink text as filename
+    note_id_func = function(title)
+      return require("obsidian.builtin").title_id(title)
+    end,
+    -- new notes go to vault root
+    new_notes_location = "notes_subdir",
+    notes_subdir = "/",
     -- preferred link style
     link = {
       style = "wiki",
-			auto_update = true,
+      auto_update = true,
     },
     -- control how notes are opened
     open_notes_in = "current",
-
     -- sort search results
     search = {
       sort_by = "modified",
       sort_reversed = true,
     },
-
-		completion = {
-			min_chars = 2,
-		},
-
+    completion = {
+      min_chars = 2,
+    },
     -- attachments (images) defaults
     attachments = {
       folder = "attachments",
@@ -37,14 +37,12 @@ return {
       img_text_func = nil,
       confirm_img_paste = true,
     },
-
     -- templates defaults
     templates = {
       folder = nil,
       date_format = nil,
       time_format = nil,
     },
-
     -- workspace
     workspaces = {
       {
@@ -52,9 +50,21 @@ return {
         path = "~/Documents/Vaulternative",
       },
     },
-		
-		picker = {
-			name = "snacks-picker"
-		}
+
+    picker = {
+      name = "snacks-picker",
+    },
+
+    -- control built-in UI rendering
+    ui = {
+      enable = true,
+    },
+
+    -- callbacks
+    callbacks = {
+      enter_note = function(note)
+        vim.opt_local.wrap = true
+      end,
+    },
   },
 }
