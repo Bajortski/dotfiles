c43f89fa3291d008570d70f0783dba64c1d46a88
Bajortski <65676366+Bajortski@users.noreply.github.com>
2026-06-08 18:36:45 -0400

    update init.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,7 +15,7 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
 if vim.g.neovide then
-  vim.o.guifont = "Maple Mono NF"
+  vim.o.guifont = "Maple Mono NF:h18"
   vim.g.neovide_opacity = 0.8
   vim.g.neovide_normal_opacity = 0.9
 end
